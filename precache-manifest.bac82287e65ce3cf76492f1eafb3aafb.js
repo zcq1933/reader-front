ad4c89fbc1eab43889d99010f60ee3590c019ae5
zcq1933 <1933748107@qq.com>
2020-06-04 22:45:27 +0800

    upload front end

---
+++ precache-manifest.bac82287e65ce3cf76492f1eafb3aafb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c8ff56d5fd6467983af6733e962c40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "435b6ce77aa1b023c0a0",
+    "url": "/static/css/2.90172c5a.chunk.css"
+  },
+  {
+    "revision": "516fc281bb6c947af412",
+    "url": "/static/css/main.70ba21e6.chunk.css"
+  },
+  {
+    "revision": "435b6ce77aa1b023c0a0",
+    "url": "/static/js/2.f34831bb.chunk.js"
+  },
+  {
+    "revision": "516fc281bb6c947af412",
+    "url": "/static/js/main.4fe0d7de.chunk.js"
+  },
+  {
+    "revision": "b7989f347b280d1dd087",
+    "url": "/static/js/runtime-main.019026b0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
